81574526433f3aa774c4b17b2a8314e8e4218e22
Kỳ <40734986+itskyf@users.noreply.github.com>
2026-05-19 10:07:18 +0700

    feat: add bitwarden ssh-agent socket

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,4 +4,7 @@ if status is-interactive
     fnox activate fish | source
     starship init fish | source
     zoxide init fish | source
+    if set --query XDG_RUNTIME_DIR
+        set --global --export SSH_AUTH_SOCK (path normalize "$XDG_RUNTIME_DIR/bitwarden-ssh-agent.sock")
+    end
 end
