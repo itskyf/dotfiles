0099f4091a976d3cc922c0aa911193607b6e0877
Kỳ <40734986+itskyf@users.noreply.github.com>
2026-05-20 13:45:55 +0700

    fix(fish): safely export SSH_AUTH_SOCK only if socket exists on WSL

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,11 +1,20 @@
 if status is-interactive
     set fish_greeting ""
+
     fish_config theme choose catppuccin-mocha
+
     fnox activate fish | source
     starship init fish | source
     zoxide init fish | source
-    if set --query XDG_RUNTIME_DIR
-        set --global --export SSH_AUTH_SOCK (path normalize "$XDG_RUNTIME_DIR/bitwarden-ssh-agent.sock")
+
+    if not set --query SSH_AUTH_SOCK
+        and set --query WSL_DISTRO_NAME
+        and set --query XDG_RUNTIME_DIR
+
+        set --local bitwarden_ssh_agent_socket (path normalize "$XDG_RUNTIME_DIR/bitwarden-ssh-agent.sock")
+
+        if test -S "$bitwarden_ssh_agent_socket"
+            set --global --export SSH_AUTH_SOCK "$bitwarden_ssh_agent_socket"
+        end
     end
 end
-
